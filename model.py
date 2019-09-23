5119a971dd4440e09154582581d18aff53bc2ab1
a1219822191 <1219822191@qq.com>
2019-09-23 08:49:35 +0800

    model

--- model.py
+++ model.py
@@ -9,24 +9,28 @@ class NET(nn.Module):
     def __init__(self):
         super(NET,self).__init__()
         self.conv1=nn.Conv1d(1,3,20)
+        self.bn2=nn.BatchNorm1d(3)
         self.ac1=nn.Sigmoid()
         self.pool1=nn.MaxPool1d(2,2)
 
         self.conv2=nn.Conv1d(3,6,20)
+        self.bn3=nn.BatchNorm1d(6)
 
-        self.linear=nn.Linear(324,32)
+        self.linear=nn.Linear(714,64)
         self.softplus=nn.Softplus()
-        self.linear1=nn.Linear(32,1)
+        self.linear1=nn.Linear(64,1)
         
         
         self.conva=nn.Conv1d(1,3,20)
-        
+        self.bn1=nn.BatchNorm1d(3)
     def forward(self, atom_one_hot,atom_env,crystal_atom_idx):
         m0,n0=atom_one_hot.shape
         atom=atom_one_hot.view(m0,1,n0)
 
         atom=self.conva(atom)
+        atom=self.bn1(atom)
         atom=F.relu(atom)
+
         s1,s2,s3=atom.shape
         atom=(atom.view(-1,s1*s2*s3).view(s1,s2*s3))
 
@@ -36,12 +40,15 @@ class NET(nn.Module):
 
 
         one=self.conv1(total)
+        one=self.bn2(one)
         one=F.relu(one)
-        one=self.pool1(one)
+        #one=self.pool1(one)
 
         one=self.conv2(one)
+        one=self.bn3(one)
         one=F.relu((one))
         one=self.pool1(one) 
+        one=F.relu(one)
 
         m1,n1,s1=one.shape
         one=(one.view(-1,m1*n1*s1).view(m1,n1*s1))
